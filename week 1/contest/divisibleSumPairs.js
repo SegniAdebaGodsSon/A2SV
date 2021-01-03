f67c82b03286aea70779022731387fd3053e3768
Segni Adeba <se.segni.adeba@gmail.com>
2021-01-03 08:56:52 +0400

    divisibleSumPairs question now running with time complexity of O(n) instead of O(n^2)

--- week 1/contest/divisibleSumPairs.js
+++ week 1/contest/divisibleSumPairs.js
@@ -1,12 +1,20 @@
 // Complete the divisibleSumPairs function below.
 function divisibleSumPairs(n, k, ar) {
     let count = 0;
-    for(let i=0; i<n; i++){
-        for(let j=i+1; j< n; j++){
-            if((ar[i] + ar[j]) % k === 0){
-                count++;
-            }
+    let modArray = ar.map(element => element%k);
+    let modCount = new Map();
+    modArray.forEach(element => {
+        if(modCount.has(k-element) || (modCount.has(0) && element === 0)){
+            count+= element === 0 ? modCount.get(element) : modCount.get(k-element);
         }
-    }
+        if(!modCount.has(element)){ 
+            modCount.set(element, 1);
+        }else{
+            let incremented = modCount.get(element) + 1;
+            modCount.set(element, incremented);
+        }     
+    });
     return count;
 }
+
+console.log(divisibleSumPairs(6, 3, [1,3,2,6,1,2]));
