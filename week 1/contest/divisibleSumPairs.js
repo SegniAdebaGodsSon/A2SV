50002d0037e94c1f5310d33d23b3653fd11f6ef6
Segni Adeba <se.segni.adeba@gmail.com>
2021-01-03 10:23:40 +0400

    repeatedStrings done

--- week 1/contest/divisibleSumPairs.js
+++ week 1/contest/divisibleSumPairs.js
@@ -16,5 +16,3 @@ function divisibleSumPairs(n, k, ar) {
     });
     return count;
 }
-
-console.log(divisibleSumPairs(6, 3, [1,3,2,6,1,2]));
--- week 1/contest/repeatedString.js
+++ week 1/contest/repeatedString.js
@@ -1,19 +1,22 @@
 // Complete the repeatedString function below.
 function repeatedString(s, n) {
-    let countS = (s) => {
+    if(s==='a') return n;
+    let countA = (s) => {
         let count = 0;
-        s.split("").forEach(element => count++);
+        s.split("").forEach(element => {
+            if(element === 'a') count++;
+            });
+        return count;
     }
-
-   let sLength = countS(s);
-   let rem = sLength % n;
-   let rep = ~~(sLength/n);
-   sLength *= rep;
-
-   for(let i=0; i<rem; i++){
-       
-   }
+    let sLen=s.length, remaining=n, repetition=0, count=1, remStr;
+    if(sLen < n){
+        repetition = Math.trunc(n/sLen);
+        remaining =  n%sLen;
+    }
+    remStr= s.substr(0, remaining);
+    count= countA(remStr)
+    if(repetition>0) count+= countA(s)*repetition; 
+    
+    return count;
 }
 
-
-console.log(repeatedString('aba', 100))
