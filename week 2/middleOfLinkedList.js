8da4bf211170f1fd7c8871b2c9c500fe75c16b4f
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2021-01-13 18:05:08 +0400

    middle of Linked List done with Tortoise and Hare approach

--- week 2/middleOfLinkedList.js
+++ week 2/middleOfLinkedList.js
@@ -5,6 +5,10 @@
  * TC - O(n), n is the number of nodes of the @param {ListNode} head
  *      O(n) to traverse every node and store the ListNodes left
  *      
+ * Solution 2
+ * TC - O(n), n is the number of nodes of the @param {ListNode} head
+ *      O(n), traversing until the fast pointer reaches the last node
+ * 
  * 
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -16,7 +20,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-// Solution 1
+// SOLUTION 1
 var middleNode1 = function(head) {
     let map = new Map(), count=0;
     curr = head;
@@ -29,3 +33,17 @@ var middleNode1 = function(head) {
     return map.get(Math.trunc(count/2));
 };
 
+// SOLUTION 2
+var middleNode2 = function(head){
+    let fast = head , slow = head;
+    while(fast.next != null && (fast.next || fast.next.next)){
+        if(!fast.next.next) {
+            fast = fast.next;
+            slow = slow.next;
+            continue;
+        }
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+}
