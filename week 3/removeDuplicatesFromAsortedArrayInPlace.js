ed2341a4c51c3ed0728be65a217f3e88e498a034
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2021-01-20 16:45:18 +0400

    removing duplicates from a sorted array , in place

---
+++ week 3/removeDuplicatesFromAsortedArrayInPlace.js
@@ -0,0 +1,34 @@
+/**
+ * link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * 
+ * Solution 1
+ * TC - O(n), n is the size of the array entered
+ *      O(n) -  traversing the whole array
+ *      O(m) -  popping duplicates
+ *      O(1) -  swap function 
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var swap = function(i, j){
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+    
+    if(!nums.length) return;
+    let len = nums.length, place=1;
+    
+    for(let i=1; i<nums.length; i++){
+        if(nums[i] !== nums[place-1]){
+            swap(i, place);
+            place++;
+        }
+    }
+
+    let count = len - place;  
+    while(count){
+        nums.pop();
+        count--;
+    }
+    return place;
+};
